fc315b7472a79f2004e000fff28987a98d0c0c44
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-08-18 21:20:31 +0700

    Add files via upload

---
+++ Number-2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+    int roll_col,num;
+
+    scanf("%d",&num);
+
+    roll_col = num -1 ;
+
+    for(int i = -roll_col ; i <= roll_col ; i++){
+        for(int j = -roll_col ; j <= roll_col ; j++){
+            if(abs(i) > (abs(j))){
+                printf(" ");
+            }else{
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
